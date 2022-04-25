de031649573653c578df0b998a9988a826541865
NickBwalley <nickbwalley@gmail.com>
2022-04-25 07:27:59 +0300

    calculating space complexity of a loop

---
+++ sum.js
@@ -0,0 +1,10 @@
+const sum = (arr) => {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr;
+  }
+  return total;
+};
+console.log(sum([3, 2, 1]));
+// running time complexity = O(n);
+// space complexity = O(1)!
