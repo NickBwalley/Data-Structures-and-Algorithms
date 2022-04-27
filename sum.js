c297c21dba01e9a19947cbbd54b1b8c7ca7f0a98
NickBwalley <nickbwalley@gmail.com>
2022-04-27 07:45:04 +0300

    determining space and time complexity of arrays

--- sum.js
+++ sum.js
@@ -1,10 +1,19 @@
 const sum = (arr) => {
   let total = 0;
   for (let i = 0; i < arr.length; i++) {
-    total += arr;
+    total += arr[i];
   }
   return total;
 };
 console.log(sum([3, 2, 1]));
 // running time complexity = O(n);
 // space complexity = O(1)!
+
+const doubleArr = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+  return newArr;
+};
+// space complexity = O(n)!
